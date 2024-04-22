ed08ca79da3274ee541929016490746820962027
lil-stupid-pig <nikitryz2001@gmail.com>
2024-04-22 21:40:36 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -120,15 +120,15 @@ class Application(QTabWidget, Ui_Widget):
             return self.answer_5.setText("Выберите каждую переменную")
 
         func = self.label_5_func.text()
-        f_x_0 = functions.residual_function(func, 0, 1)
-        f_x_1 = functions.residual_function(func, 1, 1)
-        f_y_0 = functions.residual_function(func, 0, 2)
-        f_y_1 = functions.residual_function(func, 1, 2)
-        f_z_0 = functions.residual_function(func, 0, 3)
-        f_z_1 = functions.residual_function(func, 1, 3)
-        x = 0 if f_x_0 == f_x_1 else 1
-        y = 0 if f_y_0 == f_y_1 else 1
-        z = 0 if f_z_0 == f_z_1 else 1
+        x_0 = functions.residual_function(func, 0, 1)
+        x_1 = functions.residual_function(func, 1, 1)
+        y_0 = functions.residual_function(func, 0, 2)
+        y_1 = functions.residual_function(func, 1, 2)
+        z_0 = functions.residual_function(func, 0, 3)
+        z_1 = functions.residual_function(func, 1, 3)
+        x = 0 if x_0 == x_1 else 1
+        y = 0 if y_0 == y_1 else 1
+        z = 0 if z_0 == z_1 else 1
 
         if (self.radio_button_5_x_fict.isChecked() and x == 0 or self.radio_button_5_x_su.isChecked() and x == 1) and (
                 self.radio_button_5_y_fict.isChecked() and y == 0 or self.radio_button_5_y_su.isChecked() and y == 1) and (
