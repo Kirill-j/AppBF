e9d8fa7c3ce18d74df4855331391a65efe1f97ac
lil-stupid-pig <nikitryz2001@gmail.com>
2024-04-22 11:34:29 +0800

    v0.11 Alpha

--- app/app.py
+++ app/app.py
@@ -107,6 +107,9 @@ class Application(QTabWidget, Ui_Widget):
             self.answer_4.setText("Wrong!")
 
     def exercise_5(self):
+        buttons = [self.radio_button_5_x_su, self.radio_button_5_x_fict, self.radio_button_5_y_su,
+                   self.radio_button_5_y_fict, self.radio_button_5_z_su, self.radio_button_5_z_fict]
+        for button in buttons: button.setDisabled(True)
         self.label_5_func.setText(functions.func_of_3_arg())
 
     def exercise_5_check(self):
@@ -114,6 +117,7 @@ class Application(QTabWidget, Ui_Widget):
                 self.radio_button_5_y_su.isChecked() or self.radio_button_5_y_fict.isChecked()) and (
                         self.radio_button_5_z_su.isChecked() or self.radio_button_5_z_fict.isChecked())):
             self.answer_5.setText("Выберите все переменные")
+        # self.button_g
         else:
             self.answer_5.setText("Норм")
         if self.radio_button_5_x_su.isChecked():
